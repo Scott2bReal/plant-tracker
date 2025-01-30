d76cfdfbeb9f212c7fb1d839d81374b02c6238eb
Scott Hoecker <hoeckers@gmail.com>
2025-01-30 14:37:41 -0600

    Set up solid query

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,17 @@
 /* @refresh reload */
+import { QueryClient, QueryClientProvider } from '@tanstack/solid-query'
 import { render } from 'solid-js/web'
 import App from './App.tsx'
 import './index.css'
 
 const root = document.getElementById('root')
+const client = new QueryClient()
 
-render(() => <App />, root!)
+render(
+  () => (
+    <QueryClientProvider client={client}>
+      <App />
+    </QueryClientProvider>
+  ),
+  root!
+)
