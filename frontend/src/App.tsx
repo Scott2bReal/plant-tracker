8685179911f6b9a8396b503807313472e7376253
Scott Hoecker <hoeckers@gmail.com>
2025-02-03 09:27:14 -0600

    Refactor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import Rooms from './components/Rooms'
 
 function App() {
   return (
-    <main class="p-4 text-center">
+    <main class="p-4 text-center md:p-8">
       <Rooms />
     </main>
   )
--- frontend/src/components/Rooms.tsx
+++ frontend/src/components/Rooms.tsx
@@ -3,12 +3,27 @@ import { createQuery } from '@tanstack/solid-query'
 import dayjs from 'dayjs'
 import advancedFormat from 'dayjs/plugin/advancedFormat'
 import relativeTime from 'dayjs/plugin/relativeTime'
-import { For, Show } from 'solid-js'
+import { Component, ErrorBoundary, For, Show } from 'solid-js'
 import { apiClient } from '../lib/api-client'
 import WaterRoomButton from './WaterRoomButton'
 
 dayjs.extend(relativeTime)
 dayjs.extend(advancedFormat)
+
+interface Room {
+  id: number
+  name: string
+  lastWatered: string
+}
+
+const Room: Component<Room> = (room) => (
+  <li>
+    <h4>{room.name}</h4>
+    <p>{dayjs(room.lastWatered).format('ddd, MMM Do')}</p>
+    <p>{dayjs(dayjs(room.lastWatered)).fromNow()}</p>
+    <WaterRoomButton roomId={room.id} />
+  </li>
+)
 
 const getRooms = async () => {
   const response = await apiClient<AllRoomsRouteType>().rooms.$get()
@@ -24,26 +39,18 @@ function AllRooms() {
   const queryResult = createQuery(() => ({
     queryKey: ['allRooms'],
     queryFn: getRooms,
-    staleTime: 1000 * 60 * 5, // 5 minutes
-    refetchOnWindowFocus: true,
+    staleTime: 1000 * 60, // 1 minute
   }))
 
   return (
     <>
-      <Show when={!!queryResult.data} fallback={<p>Loading...</p>}>
-        <ul class="grid grid-cols-2 gap-4 lg:grid-cols-3">
-          <For each={queryResult.data}>
-            {(room) => (
-              <li>
-                <h4>{room.name}</h4>
-                <p>{dayjs(room.lastWatered).format('ddd, MMM Do')}</p>
-                <p>{dayjs(dayjs(room.lastWatered)).fromNow()}</p>
-                <WaterRoomButton roomId={room.id} />
-              </li>
-            )}
-          </For>
-        </ul>
-      </Show>
+      <ErrorBoundary fallback={<p>Error loading rooms :(</p>}>
+        <Show when={!!queryResult.data} fallback={<p>Loading...</p>}>
+          <ul class="grid grid-cols-2 gap-4 lg:grid-cols-3">
+            <For each={queryResult.data}>{(room) => <Room {...room} />}</For>
+          </ul>
+        </Show>
+      </ErrorBoundary>
     </>
   )
 }
