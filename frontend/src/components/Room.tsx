3ffac0d7f7d223d1918717c102fb49dc6951ea07
Scott Hoecker <hoeckers@gmail.com>
2025-02-03 09:15:43 -0600

    Refactor Room component

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -1,33 +1,29 @@
 import { RoomRouteType } from '#backend/src/main'
 import { createQuery } from '@tanstack/solid-query'
-import { hc } from 'hono/client'
 import { Component, ErrorBoundary, Suspense } from 'solid-js'
+import { apiClient } from '../lib/api-client'
+
+const getRoom = async (id: number) => {
+  const response = await apiClient<RoomRouteType>().rooms[':id'].$get({
+    param: { id: String(id) },
+  })
+  if (!response.ok && response.status === 404) {
+    console.log("Connected to server, couldn't find room")
+    throw new Error(`Room with ID ${id} not found`)
+  }
+
+  if (!response.ok) {
+    console.log('Failed to fetch room')
+    throw new Error('Failed to fetch room')
+  }
+
+  return await response.json()
+}
 
 const Room: Component<{ id: number }> = (props) => {
   const queryResult = createQuery(() => ({
-    queryFn: async () => {
-      const response = await hc<RoomRouteType>(
-        import.meta.env.VITE_BACKEND_BASE_URL
-      ).rooms[':id'].$get({
-        param: { id: String(props.id) },
-      })
-
-      if (!response.ok && response.status === 404) {
-        console.log("Connected to server, couldn't find room")
-        throw new Error(`Room with ID ${props.id} not found`)
-      }
-
-      if (!response.ok) {
-        console.log('Failed to fetch room')
-        throw new Error('Failed to fetch room')
-      }
-
-      return response.json()
-    },
+    queryFn: async () => await getRoom(props.id),
     queryKey: ['room', props.id],
-    retry: 3,
-    staleTime: 1000 * 60 * 5,
-    throwOnError: true,
   }))
 
   return (
