5c787ae1da14872a42fd3f402b67b8404e6962ed
Scott Hoecker <hoeckers@gmail.com>
2025-02-03 14:47:55 -0600

    Remove water from hero

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,7 +1,6 @@
 const Hero = () => {
   return (
     <div class="mb-10 flex items-center justify-center gap-4 border-b border-cyan-900 px-8 py-4">
-      <span class="text-3xl">💧</span>
       <span class="text-7xl">🪴</span>
     </div>
   )
