92f20d736929a85c5e8231e6184a94c6835c2872
Scott Hoecker <hoeckers@gmail.com>
2025-02-23 17:26:39 -0600

    Revert changes to api client

--- frontend/src/lib/api-client.ts
+++ frontend/src/lib/api-client.ts
@@ -3,12 +3,5 @@ import { hc } from 'hono/client'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function apiClient<T extends Hono<any, any, any>>() {
-  return hc<T>(import.meta.env.VITE_BACKEND_BASE_URL, {
-    init: {
-      credentials: 'include',
-      headers: {
-        Cookie: document.cookie,
-      },
-    },
-  })
+  return hc<T>(import.meta.env.VITE_BACKEND_BASE_URL)
 }
