097257460f377c6ff007161fdf540c2f8b22fff3
Scott Hoecker <hoeckers@gmail.com>
2025-02-23 20:31:50 -0600

    Reorganize file structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,27 @@
 import { Route, Router } from '@solidjs/router'
+import { QueryClient, QueryClientProvider } from '@tanstack/solid-query'
 import { lazy } from 'solid-js'
 import LandingPage from './pages/LandingPage'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      retry: 3,
+      throwOnError: true,
+    },
+  },
+})
 
 const App = () => {
   const Login = lazy(() => import('./pages/Login'))
   return (
-    <Router>
-      <Route path="/" component={LandingPage} />
-      <Route path="/login" component={Login} />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Route path="/" component={LandingPage} />
+        <Route path="/login" component={Login} />
+      </Router>
+    </QueryClientProvider>
   )
 }
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,25 +1,7 @@
 /* @refresh reload */
-import { QueryClient, QueryClientProvider } from '@tanstack/solid-query'
 import { render } from 'solid-js/web'
 import App from './App'
 import './index.css'
 
 const root = document.getElementById('root')
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5, // 5 minutes
-      retry: 3,
-      throwOnError: true,
-    },
-  },
-})
-
-render(
-  () => (
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  ),
-  root!
-)
+render(() => <App />, root!)
